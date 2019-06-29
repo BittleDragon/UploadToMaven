20c25569b4f43843a3e5b48d18e3eda350761cf7
rxt <755284974@qq.com>
2019-06-29 16:16:55 +0800

    project init

---
+++ utilslib/src/main/java/com/rxt/utilslib/CommonUtils.kt
@@ -0,0 +1,13 @@
+package com.rxt.utilslib
+
+/**
+ * Created by raoxuting on 2019/6/29.
+ */
+class CommonUtils {
+
+    companion object {
+
+        @JvmStatic
+        fun getSystemTime() = System.currentTimeMillis()
+    }
+}
